8b634f8e08480c63fa0b1363eb61e96b6652a1d3
Victor Monje <monjev086@gmail.com>
2024-02-18 22:00:47 -0600

    modify page auth and add shandcn

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         {/* {`public routes`} */}
         <Route element={<AuthLayout />} >
-          <Route path="/sign-up" element={<SigninForm />} />
+          <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
         {/* {`private routes`} */}
